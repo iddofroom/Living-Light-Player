8f3384586e3d5cbb71912bdfbb7298d9e7f9a685
Amir Blum <amirgiraffe@gmail.com>
2024-12-14 21:09:59 +0200

    pio

--- Burnerot24Leds/src/main.cpp
+++ Burnerot24Leds/src/main.cpp
@@ -1,6 +1,20 @@
 
  #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
+#include "SdLedsPlayer.h"
+
+#define LEDS_PER_STRIP 254
+
+/*
+ * SdLedsPlayer is the class that handles reading frames from file on SD card,
+ * and writing it to the leds.
+ * It needs to be initialized with LEDS_PER_STRIP (must match the leds per strips used in the generation
+ * of the file written to SD card).
+ * It also needs to receive the leds buffer for OctoWS2811, should be initialized as follows
+ */
+DMAMEM int display_memory[LEDS_PER_STRIP * 6]; 
+int drawing_memory[LEDS_PER_STRIP * 6];
+SdLedsPlayer sd_leds_player(LEDS_PER_STRIP, display_memory, drawing_memory);
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
@@ -17,6 +31,13 @@ const long interval = 500;  // כל 5 שניות
 
 void setup() {
   
+  while (! sd_leds_player.setup()) {
+    Serial.println("SD card setup failed, fix and reset to continue");
+    delay(1000);
+  }
+  Serial.println("SD card started.");
+  sd_leds_player.setBrightness(128);
+
   // הגדרת פינים ככניסות
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
